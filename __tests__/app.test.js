e506154644b88b72c080bc424fc36881daf6c84a
Matthew Saber <mattsaber21@yahoo.com>
2021-05-19 14:13:06 -0700

    push

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,5 +1,7 @@
-import app from '../lib/app.js';
-import supertest from 'supertest';
+// import { weather } from '../data/weather.js';
+// import { location } from '../data/location.js';
+// import { reviews } from '../data/reviews.js';
+// import { formatWeather, formatLocation, formatReviews } from '../lib/munge-utils.js';
 
 //const request = supertest(app);
 
