da1a9a4301962cb04a401d3b31bab9a44e7a4e12
Matthew Saber <mattsaber21@yahoo.com>
2021-05-20 08:26:22 -0700

    map working

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,11 +1,11 @@
-// import { weather } from '../data/weather.js';
-// import { location } from '../data/location.js';
-// import { reviews } from '../data/reviews.js';
-// import { formatWeather, formatLocation, formatReviews } from '../lib/munge-utils.js';
+import { weather } from '../data/weather.js';
+import { location } from '../data/location.js';
+import { reviews } from '../data/reviews.js';
+import { formatWeather, formatLocation, formatReviews } from '../lib/munge-utils.js';
 
-//const request = supertest(app);
+const request = supertest(app);
 
 describe('API Routes', () => {
-  it('', async () => {
+  it('test location', async () => {
   });
 });
