9317c5a68e068afa6157e659403dfe30f7d93f6e
Matthew Saber <mattsaber21@yahoo.com>
2021-05-20 08:46:52 -0700

    location test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,11 +1,20 @@
-import { weather } from '../data/weather.js';
-import { location } from '../data/location.js';
-import { reviews } from '../data/reviews.js';
-import { formatWeather, formatLocation, formatReviews } from '../lib/munge-utils.js';
+//import { weather } from '../lib/data/weather.js';
+import locationData from '../lib/data/location';
+//import { reviews } from '../lib/data/reviews.js';
+import { formatWeather, formatLocation, formatReviews } from '../lib/mungeUtils.js';
 
-const request = supertest(app);
+//const request = supertest(app);
 
 describe('API Routes', () => {
   it('test location', async () => {
+    const expected = {
+      formatted_query: 'Boston, Suffolk County, Massachusetts, USA',
+      latitude: '42.3602534',
+      longitude: '-71.0582912'
+    };
+
+    const actual = formatLocation(locationData);
+
+    expect(expected).toEqual(actual);
   });
 });
